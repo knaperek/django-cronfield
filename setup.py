1a6902e60609930984dbd822b1d7fad05033b2df
Jozef Knaperek <jknaperek@gmail.com>
2014-03-08 20:57:03 +0100

    Bump version to 0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-cronfield',
-    version='0.2dev',
+    version='0.2',
     packages=['cronfield'],
     include_package_data=True,
     license='BSD License',
